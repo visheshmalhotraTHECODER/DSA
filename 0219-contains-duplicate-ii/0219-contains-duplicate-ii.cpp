62a6fdedc90c89a40bcd7ab35edf9afaffb39014
Vishesh Malhotra <visheshmalhotra2023@gmail.com>
2026-07-11 11:53:02 +0530

    Time: 69 ms (80.1%), Space: 98.8 MB (13.69%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int>jhola;
+
+        for(int i = 0; i<nums.size(); i++){
+
+           if(jhola.find(nums[i])!=jhola.end()){
+            if(i - jhola[nums[i]]<=k){
+                return true;
+            }
+
+           }
+           jhola[nums[i]] = i ;
+        }
+        return false;
+        
+    }
+};
