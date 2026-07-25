e6af7ec5fdac3078f543d697df31f758c4a45908
Vishesh Malhotra <visheshmalhotra2023@gmail.com>
2026-07-25 16:33:35 +0530

    Time: 72 ms (8.16%), Space: 34 MB (12.34%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -4,19 +4,36 @@ public:
 
         unordered_map<string, vector<string>> jhola;
 
-        for (int i = 0; i < strs.size(); i++) {
+        for (string s : strs) {
+            int count[26];
 
-            string word = strs[i];
+            for (int i = 0; i < 26; i++) {
+                count[i] = 0;
+            }
 
-            sort(word.begin(), word.end());
+            for (char ch : s) {
+                count[ch - 'a']++;
+            }
+            string key = "";
 
-            jhola[word].push_back(strs[i]);
-        }
-        vector<vector<string>> ans;
+            for (int i = 0; i < 26; i++) {
+                key += '#';
+                key += to_string(count[i]);
+            }
+            if (jhola.find(key) == jhola.end()) {
 
-        for (auto item : jhola) {
-            ans.push_back(item.second);
-        }
+                jhola[key] = {};
+            }
+
+                jhola[key].push_back(s);
+            }
+            vector<vector<string>>ans;
+
+            for (auto item : jhola) {
+                ans.push_back(item.second);
+            }
+            
         return ans;
+       
     }
 };
