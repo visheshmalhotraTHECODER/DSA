fa77267a222d3a22a8931bb7c1e8bf06d150c7ea
Vishesh Malhotra <visheshmalhotra2023@gmail.com>
2026-08-16 17:22:52 +0530

    Time: 4 ms (20.94%), Space: 40.3 MB (45.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,20 +4,17 @@ public:
         vector<int>ans(nums.size(),1);
 
         int pre = 1;
-        int post =1;
+        int post = 1;
 
+        for(int i = 0 ;i <nums.size(); i++){
+            ans[i] = pre;
+            pre = nums[i]*pre;
 
-        for(int i = 0; i<nums.size(); i++){
-
-            ans[i] =pre;
-            pre =nums[i]*pre;
-            
         }
-        for(int i = nums.size()-1; i>=0;i--){
-            ans[i] = post*ans[i];
-            post =post*nums[i];
+        for(int i = nums.size()-1; i>=0; i--){
+            ans[i] = post * ans[i];
+            post = post *nums[i];
         }
         return ans;
-        
     }
 };
