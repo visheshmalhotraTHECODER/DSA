7684dda017d8196f4ff3f24f98e681dd383b605d
Vishesh Malhotra <visheshmalhotra2023@gmail.com>
2026-08-17 18:05:57 +0530

    Time: 0 ms (100%), Space: 10 MB (53.85%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -6,20 +6,19 @@ public:
         int right = s.length()-1;
 
         while(left<right){
+
             while(left<right && !isalnum(s[left])){
                 left++;
             }
             while(left<right && !isalnum(s[right])){
                 right--;
             }
-            if(tolower(s[left])!=tolower(s[right])){
-                return false;
+            if(tolower(s[left])!= tolower(s[right])){
+              return false;  
             }
             left++;
             right--;
-
         }
         return true;
-        
     }
 };
