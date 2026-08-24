77ec768f32d4b956911cf485d4f6fbb23272c011
Vishesh Malhotra <visheshmalhotra2023@gmail.com>
2026-08-24 10:42:59 +0530

    Time: 333 ms (35.12%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -5,4 +5,12 @@ SELECT
 FROM
     World
 WHERE
-    area >= 3000000 OR population >= 25000000      
+    area >= 3000000
+
+UNION
+SELECT
+    name, population , area
+FROM
+    World
+WHERE
+    population >= 25000000            
