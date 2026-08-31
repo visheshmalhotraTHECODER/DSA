7b817b103242e167ef5c2e639d816047963f747b
Vishesh Malhotra <visheshmalhotra2023@gmail.com>
2026-08-31 22:47:46 +0530

    Time: 2 ms (5.11%), Space: 7.9 MB (74.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        
         if(n<=2){
             return n;
         }
-       int a = 1; 
-       int b = 2;
+        int a = 1;
+        int b = 2;
+        for(int  i = 3; i<=n; i++){
 
-       for(int  i = 3; i<=n; i++){
-        int c  =  a+ b;
-        a = b;
-        b = c;
-       }
-       return b;
+            int c = a+b;
+
+            a= b;
+            b=c;
+
+        }
+        return b;
     }
 };
