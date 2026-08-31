f51b99dd1f1dce771cf75382206f0e41cf681eb8
Vishesh Malhotra <visheshmalhotra2023@gmail.com>
2026-08-31 22:41:12 +0530

    Time: 0 ms (100%), Space: 7.8 MB (88.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-
-    int solve(int n,vector<int>& dp ){
-        if(n == 0){// basically apan end p phauch gyue
-            return 1;
-        }
-        if(n == 1){
-            return 1;
-        }
-        if(dp[n]!= -1){
-            return dp[n];
-
-        }
-        int onestep= solve(n-1, dp);
-        int twostep = solve(n-2, dp);
-
-        dp[n] = onestep + twostep;
-
-        return dp[n];
-    }
-
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
-        return solve(n,dp);
-        
+        if(n<=2){
+            return n;
+        }
+       int a = 1; 
+       int b = 2;
+
+       for(int  i = 3; i<=n; i++){
+        int c  =  a+ b;
+        a = b;
+        b = c;
+       }
+       return b;
     }
 };
