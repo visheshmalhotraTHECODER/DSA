24a84a1736867ed36b6034210d7a673c2d5c2239
Vishesh Malhotra <visheshmalhotra2023@gmail.com>
2026-09-03 21:49:20 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        
+    int solve(vector<int> &t, int n){
         if(n<=2){
             return n;
         }
-        int a = 1;
-        int b = 2;
-        for(int  i = 3; i<=n; i++){
-
-            int c = a+b;
-
-            a= b;
-            b=c;
-
+        if(t[n]!= -1){
+            return t[n];
         }
-        return b;
+        return t[n] = solve(t, n-1)+solve(t,n-2); 
+        
+    }
+    int climbStairs(int n) {
+        vector<int>t(n+1,-1);
+
+        return solve(t, n);
     }
 };
