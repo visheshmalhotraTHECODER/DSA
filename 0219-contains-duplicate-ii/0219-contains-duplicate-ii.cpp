63ccd676d2b5e73053965e269dfbfb8d43cf6608
Vishesh Malhotra <visheshmalhotra2023@gmail.com>
2026-09-05 02:12:58 +0530

    Time: 129 ms (17.5%), Space: 134.2 MB (22.86%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -6,13 +6,15 @@ public:
         int n = nums.size();
 
         for (int i = 0; i < n; i++) {
-            if (jhola.find(nums[i]) != jhola.end()) {
-                if (i - jhola[nums[i]] <= k)
-                    return true;
-            }
+            if (jhola.find(nums[i]) != jhola.end()) 
+                return true;
 
             jhola[nums[i]] = i;
-        }
+
+          if(i>=k){
+            jhola.erase(nums[i-k]);
+        }    
+    }
         return false;
     }
 };
