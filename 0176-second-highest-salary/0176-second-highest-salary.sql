8334d0865974b6bc47f9226317168cac7dc14b50
Vishesh Malhotra <visheshmalhotra2023@gmail.com>
2026-09-08 19:53:52 +0530

    Time: 333 ms (27.74%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT MAX(e1.salary) AS SecondHighestSalary 
 FROM Employee e1
-INNER JOIN  Employee e2
-ON e1.salary < e2.salary
+INNER JOIN Employee e2
+WHERE e1.salary < e2.salary
