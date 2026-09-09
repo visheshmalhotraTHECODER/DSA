31e0b5bf6deadb5636c0bfad3bfe42f78fe409af
Vishesh Malhotra <visheshmalhotra2023@gmail.com>
2026-09-09 14:50:40 +0530

    Time: 0 ms (100%), Space: 12.5 MB (54.72%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -9,22 +9,19 @@ public:
             if(nums1[i]>nums2[j]){
                 nums1[k] = nums1[i];
                 i--;
-                
-
             }
             else{
                 nums1[k] = nums2[j];
                 j--;
-                
             }
             k--;
+            
         }
-            while(j>=0){
+        while(j>=0){
                 nums1[k] = nums2[j];
                 j--;
                 k--;
             }
         
-
     }
 };
