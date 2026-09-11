8627a7de7467543e0e66ba34e95c8154020f4e49
Vishesh Malhotra <visheshmalhotra2023@gmail.com>
2026-09-12 02:53:30 +0530

    Time: 0 ms (100%), Space: 18 MB (76.04%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
 
         unordered_map<int,int>jhola;
 
         for(int num:nums){
             jhola[num]++;
         }
-
-        
         priority_queue<pair<int,int>>pq;
 
-        for(auto item : jhola){
+        for(auto item: jhola){
             pq.push({item.second,item.first});
         }
         vector<int>ans;
 
-        while(k--){;
-
-        ans.push_back(pq.top().second);
-        pq.pop();
+        while(k>0){
+            ans.push_back(pq.top().second);
+            pq.pop();
+            k--;
         }
         return ans;
-
+        
     }
 };
