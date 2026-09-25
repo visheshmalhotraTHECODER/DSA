5382f007661994a86fffb78228d6c397f8c2fbe8
Vishesh Malhotra <visheshmalhotra2023@gmail.com>
2026-09-25 22:14:57 +0530

    Time: 35 ms (37.78%), Space: 14 MB (99.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        unordered_map<int,int>jhola;
 
-        for(int i = 0; i<n ; i++){
-            int required = target - nums[i];
-            if(jhola.find(required)!= jhola.end()){
-                return {jhola[required], i};
-            }
-            else{
-                jhola[nums[i]] = i;
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
             }
         }
         return {};
